1990f2c48c784825bad2b930822552e4a04cbca7
Aerovek Community <122138345+AerovekCommunity@users.noreply.github.com>
2023-06-14 20:25:51 +0200

    Update easa.pdf.tsx

--- PilotLogs/EASA/easa.pdf.tsx
+++ PilotLogs/EASA/easa.pdf.tsx
@@ -1,1 +1,27 @@
+import React from "react";
+import { Document, Page, Text, View, StyleSheet, PDFViewer, PDFDownloadLink } from "@react-pdf/renderer";
 
+const EASAPDF = () => {
+  return (
+    <div>
+      <PDFDownloadLink document={<TableDocument />} fileName="easa.pdf">
+        {({ blob, url, loading, error }) => (loading ? "Loading document..." : "Download now!")}
+      </PDFDownloadLink>
+      <PDFViewer>
+        <TableDocument />
+      </PDFViewer>
+    </div>
+  );
+};
+
+const TableDocument = () => (
+  <Document>
+    <Page size="A4" orientation="landscape">
+      <View style={styles.table}>
+        //Create PDF Document
+      </View>
+    </Page>
+  </Document>
+);
+
+export default EASAPDF;
